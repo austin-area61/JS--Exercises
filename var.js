fa4ef58273b811106f42d21cffe630f9f1ed1371
austin-area61 <austinonyango.area61@gmail.com>
2024-06-29 14:25:37 +0300

    Practicing variables using the variable name 'let'.

---
+++ var.js
@@ -0,0 +1,10 @@
+let variable1 = 3;
+console.log(variable1);
+
+let calculation = 2 + 3;
+console.log(calculation);
+console.log(variable1 + calculation);
+let result = variable1 + calculation;
+console.log(result);
+let message = 'hello';
+console.log(message);

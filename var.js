82360703d47a0921136e188f0467797b7e8bbc36
austin-area61 <austinonyango.area61@gmail.com>
2024-07-01 18:47:23 +0300

    -Looked at different ways to name variables and which is more convenient based on the state of the value, i.e whether they will be re-assigned or not.

--- var.js
+++ var.js
@@ -1,17 +1,22 @@
-let variable1 = 3;
+let variable1 = 3; //use let because it will be re-assigned later on
 console.log(variable1);
 
-let calculation = 2 + 3;
+const calculation = 2 + 3; //using const because it will not be re-assigned
 console.log(calculation);
 console.log(variable1 + calculation);
-let result = variable1 + calculation;
+
+const result = variable1 + calculation;
 console.log(result);
-let message = 'hello';
+
+const message = 'hello';
 console.log(message); 
 console.log(';');
 
-/*let*/variable1 = 5;
+variable1 = 5;
 console.log(variable1);
 
 variable1 = variable1 + 1;
 console.log(variable1);
+
+var variable3 = 3; //var can also be re-assigned
+console.log(typeof variable3);

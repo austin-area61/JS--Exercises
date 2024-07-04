7fd24fe90eda9b45d46680a70b34e5f623643415
austin-area61 <austinonyango.area61@gmail.com>
2024-07-04 18:39:56 +0300

    Learning how if statements work together with the boolean values true and false.

--- Scripts/boolean.js
+++ Scripts/boolean.js
@@ -1,4 +1,17 @@
 true
 false
+console.log(false);
+console.log(3 > 5);
+console.log(5 === '5.0')
 
-3 < 5
+if(true){
+  console.log('hello world!');
+} else{
+  console.log('goodbye world!');
+}
+let age = 15
+if(age >= 16){
+  console.log('you can drive');
+} else {
+  console.log('you cannot drive');
+}

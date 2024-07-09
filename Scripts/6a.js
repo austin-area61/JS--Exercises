b15559ad9be0a17d1c20ddd3fca6111c34409f29
austin-area61 <austinonyango.area61@gmail.com>
2024-07-09 15:26:56 +0300

    a simple if else statement that displays a spedific message depending on what time it is.

---
+++ Scripts/6a.js
@@ -0,0 +1,9 @@
+let hour = 23;
+
+if(hour >= 6 && hour <= 12){
+ console.log("Good Morning");
+} else if (hour >= 13 && hour <= 17){
+  console.log("Good Afternoon");
+} else{
+  console.log("Good night");
+}
